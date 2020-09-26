446a5ffa6562752e3c7aa040e6696df97b4c50b9
Nikitosis <nikitospupov@gmail.com>
2020-09-26 10:47:13 +0300

    Init

---
+++ lab1/server/src/main/java/com/server/ServerApplication.java
@@ -0,0 +1,12 @@
+package com.server;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
